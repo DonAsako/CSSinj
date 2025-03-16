214ddeee2a7ed7f80101891dd2dac5fd1c9f2abf
DonAsako <asakosan.pro@protonmail.com>
2025-03-16 23:01:11 +0100

    refactor remove Elements

--- cssinj/dom.py
+++ cssinj/dom.py
@@ -1,6 +1,6 @@
 import dataclasses
 from typing import Optional
-
+from collections.abc import MutableSequence
 
 @dataclasses.dataclass
 class Attribut:
@@ -19,7 +19,7 @@ class Attribut:
 class Element:
     id: int = dataclasses.field(default=0, init=False)
     name: str
-    parent: Optional["Element"]
+    parent: Optional["Element"] = None
     attributs: list = dataclasses.field(default_factory=list)
     children: list = dataclasses.field(default_factory=list)
     _id_counter: int = dataclasses.field(default=0, init=False, repr=False)
@@ -31,49 +31,3 @@ class Element:
         # Add element to children list of his parent
         if self.parent:
             self.parent.children.append(self)
-
-class Elements(MutableSequence):
-    def __init__(self):
-        super().__init__()
-        self.element_list = []
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__} elements: {repr(self.element_list)}>"
-
-    def __contains__(self, value):
-        return value in self.element_list
-
-    def __len__(self):
-        return len(self.element_list)
-
-    def __getitem__(self, id):
-        element = self.get_elemnt_by_id(id)
-        return element
-
-    def __delitem__(self, id):
-        item = self.get_element_by_id(id)
-        self.element_list.remove(item)
-        return item
-
-    def __setitem__(self, i, element):
-        self.element_list.append(element)
-
-    def append(self, element):
-        self.element_list.append(element)
-
-    def insert(self, id, new_element):
-        for i in range(len(self.element_list)):
-            if self.element_list[i].id == id:
-                self.element_list[i] = new_element
-
-    def __add__(self, another_elements):
-        if isinstance(another_elements, elements):
-            return self.__class__(self.element_list + another_elements)
-
-    def get_element_by_id(self, id):
-        for element in self.element_list:
-            if element.id == int(id):
-                return element
-
-    def clear(self):
-        self.element_list.clear()
