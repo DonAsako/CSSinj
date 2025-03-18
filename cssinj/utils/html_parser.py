a5af1207eaebce0534bda1c39168209388497ca9
DonAsako <asakosan.pro@protonmail.com>
2025-03-18 12:33:29 +0100

    Add getter

--- cssinj/utils/html_parser.py
+++ cssinj/utils/html_parser.py
@@ -28,3 +28,11 @@ class HtmlParser(HTMLParser):
                     elements_by_attr.append(element)
 
         return elements_by_attr
+
+    def get_all_value_by_attr_name(self, attr_name) -> list:
+        elements_by_attr = self.get_all_value_by_attr(attr_name)
+        values = []
+        for element in elements_by_attr:
+            for attribut in element.attributs:
+                if attribut.name = attr_name:
+                    values.append(attribut.value)
